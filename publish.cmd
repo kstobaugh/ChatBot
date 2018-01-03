064f92c4693f7b4ea675dab0517d7b98dde046f0
kstobaugh <34344227+kstobaugh@users.noreply.github.com>
2018-01-03 13:38:14 -0600

    Add project files.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=chatadventureworksproducts-Web-Deploy.pubxml -p:Password=ufkL27c39b3xRtja4XLhApqJ1vuWHq88R9S5kdW8asPa5noHApkh3R0ntfYz
+
